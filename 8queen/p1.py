0df7b24dba7ede1554a731b7e24b65ec7aa67f3a
Haye Chan <hayechan@Hayes-MacBook-Pro-3.local>
2026-04-15 14:36:23 +0800

    added 8queen

---
+++ 8queen/p1.py
@@ -0,0 +1,22 @@
+from itertools import permutations as perm
+
+def check(config):
+    setd0 = set()
+    setd1 = set()
+    for c in range(N):
+        d0 = c + config[c]
+        d1 = c - config[c]
+        if (d0 in setd0 or d1 in setd1):
+            return False
+        setd0.add(d0)
+        setd1.add(d1)
+    return True
+
+N = 10
+solution = 0
+for p in perm(range(N)):
+    if check(p):
+        # print(p)
+        solution += 1
+
+print(solution)
---
+++ 8queen/v1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+vector<int> Config;
+bool Diag0[200], Diag1[200];
+int N;
+bool check() {
+	int i, j;
+	for (i=0; i<2*N + 1; i++) {
+		Diag0[i] = false;
+		Diag1[i] = false;
+	}
+	for (i=0; i<N; i++) {
+		int d0 = i + Config[i];
+		int d1 = i - Config[i] + N;
+		
+		if (Diag0[d0] || Diag1[d1])
+			return false;
+		Diag0[d0] = true;
+		Diag1[d1] = true;
+	}
+	return true;
+}
+
+int main() {
+	int i, j;
+	int solutions = 0;
+	cin >> N;
+	for (i = 0; i<N; i++)
+		Config.push_back(i);
+	while (next_permutation(Config.begin(), Config.end())) {			
+		if (check())
+			solutions++;
+		// for (auto x: Config)
+		// 	cout << x << " ";
+		// cout << endl;
+	}
+	cout << solutions << endl;
+	return 0;
+}
